73d883b72f97338a420831a8ff7d7498052caaf7
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-09-08 20:58:07 -0400

    ‘Client::dispatch_events’: pass ‘self’ to callbacks
    This allows the callbacks to perform I/O on the vchan.

--- qubes-gui-client/src/agent.rs
+++ qubes-gui-client/src/agent.rs
@@ -12,47 +12,86 @@ pub use io::*;
 /// The trait that a GUI agent must implement.
 pub trait AgentTrait {
     /// Called when a motion event is received on the vchan
-    fn motion(&mut self, window: u32, event: qubes_gui::Motion) -> std::io::Result<()>;
+    fn motion(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        event: qubes_gui::Motion,
+    ) -> std::io::Result<()>;
 
     /// The pointer has entered or left a window.
-    fn crossing(&mut self, window: u32, event: qubes_gui::Crossing) -> std::io::Result<()>;
+    fn crossing(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        event: qubes_gui::Crossing,
+    ) -> std::io::Result<()>;
 
     /// The user wishes to close a window
-    fn close(&mut self, window: u32) -> std::io::Result<()>;
+    fn close(&mut self, agent: &mut super::Client, window: u32) -> std::io::Result<()>;
 
     /// A key has been pressed or released
-    fn keypress(&mut self, window: u32, event: qubes_gui::Keypress) -> std::io::Result<()>;
+    fn keypress(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        event: qubes_gui::Keypress,
+    ) -> std::io::Result<()>;
 
     /// A button has been pressed or released
-    fn button(&mut self, window: u32, button: qubes_gui::Button) -> std::io::Result<()>;
+    fn button(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        button: qubes_gui::Button,
+    ) -> std::io::Result<()>;
 
     /// The GUI daemon has requested the contents of the clipboard.  The agent
     /// is expected to send a [`qubes_gui::MSG_CLIPBOARD_DATA`] message with the
     /// requested data.
-    fn copy(&mut self) -> std::io::Result<()>;
+    fn copy(&mut self, agent: &mut super::Client) -> std::io::Result<()>;
 
     /// Set the contents of the clipboard.
-    fn paste(&mut self, paste_buffer: &str) -> std::io::Result<()>;
+    fn paste(&mut self, agent: &mut super::Client, paste_buffer: String) -> std::io::Result<()>;
 
     /// The keymap has changed.
-    fn keymap(&mut self, keymap: qubes_gui::KeymapNotify) -> std::io::Result<()>;
+    fn keymap(
+        &mut self,
+        agent: &mut super::Client,
+        keymap: qubes_gui::KeymapNotify,
+    ) -> std::io::Result<()>;
 
     /// The agent must redraw a portion of the display
-    fn redraw(&mut self, window: u32, portion_to_redraw: qubes_gui::MapInfo)
-        -> std::io::Result<()>;
+    fn redraw(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        portion_to_redraw: qubes_gui::MapInfo,
+    ) -> std::io::Result<()>;
 
     /// A window has been moved and/or resized.
     fn configure(
         &mut self,
+        agent: &mut super::Client,
         window: u32,
         new_size_and_positon: qubes_gui::Configure,
     ) -> std::io::Result<()>;
 
     /// A window has gained or lost focus
-    fn focus(&mut self, window: u32, event: qubes_gui::Focus) -> std::io::Result<()>;
+    fn focus(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        event: qubes_gui::Focus,
+    ) -> std::io::Result<()>;
 
     /// Window manager flags have changed
-    fn window_flags(&mut self, window: u32, flags: qubes_gui::WindowFlags) -> std::io::Result<()>;
+    fn window_flags(
+        &mut self,
+        agent: &mut super::Client,
+        window: u32,
+        flags: qubes_gui::WindowFlags,
+    ) -> std::io::Result<()>;
 }
 
 impl super::Client {
@@ -73,57 +112,60 @@ impl super::Client {
                 qubes_gui::MSG_MOTION => {
                     let mut m = qubes_gui::Motion::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.motion(header.window, m)?
+                    implementation.motion(self, header.window, m)?
                 }
                 qubes_gui::MSG_CROSSING => {
                     let mut m = qubes_gui::Crossing::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.crossing(header.window, m)?
+                    implementation.crossing(self, header.window, m)?
                 }
                 qubes_gui::MSG_CLOSE => {
                     assert!(body.is_empty());
-                    implementation.close(header.window)?
+                    implementation.close(self, header.window)?
                 }
                 qubes_gui::MSG_KEYPRESS => {
                     let mut m = qubes_gui::Keypress::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.keypress(header.window, m)?
+                    implementation.keypress(self, header.window, m)?
                 }
                 qubes_gui::MSG_BUTTON => {
                     let mut m = qubes_gui::Button::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.button(header.window, m)?
+                    implementation.button(self, header.window, m)?
                 }
-                qubes_gui::MSG_CLIPBOARD_REQ => implementation.copy()?,
+                qubes_gui::MSG_CLIPBOARD_REQ => implementation.copy(self)?,
                 qubes_gui::MSG_CLIPBOARD_DATA => {
-                    implementation.paste(std::str::from_utf8(body).map_err(|e| {
-                        std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string())
-                    })?)?
+                    let paste_data = std::str::from_utf8(body)
+                        .map_err(|e| {
+                            std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string())
+                        })?
+                        .to_owned();
+                    implementation.paste(self, paste_data)?
                 }
                 qubes_gui::MSG_KEYMAP_NOTIFY => {
                     let mut m = qubes_gui::KeymapNotify::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.keymap(m)?
+                    implementation.keymap(self, m)?
                 }
                 qubes_gui::MSG_MAP => {
                     let mut m = qubes_gui::MapInfo::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.redraw(header.window, m)?
+                    implementation.redraw(self, header.window, m)?
                 }
                 qubes_gui::MSG_CONFIGURE => {
                     let mut m = qubes_gui::Configure::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.configure(header.window, m)?
+                    implementation.configure(self, header.window, m)?
                 }
                 qubes_gui::MSG_FOCUS => {
                     let mut m = qubes_gui::Focus::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.focus(header.window, m)?
+                    implementation.focus(self, header.window, m)?
                 }
                 qubes_gui::MSG_WINDOW_FLAGS => {
                     let mut m = qubes_gui::WindowFlags::default();
                     m.as_mut_bytes().copy_from_slice(body);
-                    implementation.window_flags(header.window, m)?
+                    implementation.window_flags(self, header.window, m)?
                 }
                 _ => {}
             }
