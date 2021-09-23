387940f1239edbaabaa3aca83f207ede25c31343
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-09-22 20:04:53 -0400

    Add unit tests for vchan writing routines
    This ensures that the data written to the vchan is correct.

--- qubes-gui-client/src/buffer.rs
+++ qubes-gui-client/src/buffer.rs
@@ -24,7 +24,7 @@
 use qubes_castable::Castable as _;
 use qubes_gui::Header;
 use std::collections::VecDeque;
-use std::io::{self, Error, ErrorKind, Read, Write};
+use std::io::{self, Error, ErrorKind};
 use std::mem::size_of;
 use std::ops::Range;
 
@@ -37,9 +37,10 @@ enum ReadState {
 }
 
 // Trait for a vchan, for unit-testing
-pub(crate) trait VchanMock: Read + Write {
+pub(crate) trait VchanMock {
     fn buffer_space(&self) -> usize;
     fn recv(&mut self, buf: &mut [u8]) -> io::Result<usize>;
+    fn send(&mut self, buf: &[u8]) -> io::Result<usize>;
     fn wait(&self);
     fn data_ready(&self) -> usize;
 }
@@ -50,6 +51,9 @@ impl VchanMock for vchan::Vchan {
     }
     fn recv(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         vchan::Vchan::recv(self, buf)
+    }
+    fn send(&mut self, buf: &[u8]) -> io::Result<usize> {
+        vchan::Vchan::send(self, buf)
     }
     fn wait(&self) {
         vchan::Vchan::wait(self)
@@ -87,7 +91,7 @@ impl<T: VchanMock> Vchan<T> {
             Ok(0)
         } else {
             let to_write = space.min(slice.len());
-            vchan.write(&slice[..to_write])
+            vchan.send(&slice[..to_write])
         }
     }
 
@@ -268,5 +272,118 @@ impl Vchan<vchan::Vchan> {
 
     pub fn as_raw_fd(&self) -> std::os::raw::c_int {
         self.vchan.fd()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    struct MockVchan {
+        read_buf: Vec<u8>,
+        write_buf: Vec<u8>,
+        buffer_space: usize,
+        data_ready: usize,
+        cursor: usize,
+    }
+
+    impl VchanMock for MockVchan {
+        fn wait(&self) {}
+        fn data_ready(&self) -> usize {
+            self.data_ready
+        }
+        fn buffer_space(&self) -> usize {
+            self.buffer_space
+        }
+        fn send(&mut self, buffer: &[u8]) -> io::Result<usize> {
+            assert!(
+                buffer.len() <= self.buffer_space,
+                "Agents never write more space than is available"
+            );
+            self.write_buf.extend_from_slice(buffer);
+            self.buffer_space -= buffer.len();
+            Ok(buffer.len())
+        }
+        fn recv(&mut self, buffer: &mut [u8]) -> io::Result<usize> {
+            assert!(
+                self.read_buf.len() >= self.data_ready
+                    && self.read_buf.len() - self.data_ready >= self.cursor,
+                "mock vchan internal bounds error"
+            );
+            assert!(
+                buffer.len() <= self.data_ready,
+                "Agents never read more data than is available"
+            );
+            buffer.copy_from_slice(&self.read_buf[self.cursor..self.cursor + buffer.len()]);
+            self.cursor += buffer.len();
+            Ok(buffer.len())
+        }
+    }
+    #[test]
+    fn vchan_data() {
+        let mock_vchan = MockVchan {
+            read_buf: vec![],
+            write_buf: vec![],
+            buffer_space: 0,
+            data_ready: 0,
+            cursor: 0,
+        };
+        let mut under_test = Vchan::<MockVchan> {
+            vchan: mock_vchan,
+            queue: Default::default(),
+            offset: 0,
+            header: Default::default(),
+            state: ReadState::ReadingHeader,
+            buffer: vec![],
+        };
+        under_test.write(b"test1").unwrap();
+        assert_eq!(under_test.queue.len(), 1, "message queued");
+        assert_eq!(under_test.queue[0], b"test1");
+        assert_eq!(under_test.offset, 0, "no bytes written");
+        assert_eq!(under_test.vchan.write_buf, b"", "no bytes written");
+        under_test.vchan.buffer_space = 3;
+        under_test.drain().expect("drained successfully");
+        assert_eq!(under_test.queue.len(), 1);
+        assert_eq!(under_test.queue[0], b"test1");
+        assert_eq!(under_test.offset, 3);
+        assert_eq!(under_test.vchan.write_buf, b"tes");
+        assert_eq!(under_test.vchan.buffer_space, 0);
+        under_test.vchan.buffer_space = 4;
+        under_test.write(b"\0another alpha").unwrap();
+        assert_eq!(under_test.queue.len(), 1);
+        assert_eq!(under_test.vchan.write_buf, b"test1\0a");
+        assert_eq!(
+            under_test.queue[0], b"nother alpha",
+            "only the minimum number of bytes stored"
+        );
+        assert_eq!(under_test.offset, 0);
+        under_test.vchan.buffer_space = 2;
+        under_test.drain().expect("drained successfully");
+        assert_eq!(under_test.vchan.write_buf, b"test1\0ano");
+        assert_eq!(under_test.vchan.buffer_space, 0);
+        under_test.vchan.buffer_space = 7;
+        assert_eq!(under_test.read_header().unwrap(), None, "no bytes to read");
+        assert_eq!(under_test.vchan.buffer_space, 0);
+        assert_eq!(under_test.vchan.write_buf, b"test1\0another al");
+        assert_eq!(under_test.queue.len(), 1);
+        assert_eq!(under_test.queue[0], b"nother alpha");
+        assert_eq!(under_test.offset, 9);
+        under_test.vchan.buffer_space = 8;
+        under_test.write(b" gamma delta").expect("write works");
+        assert_eq!(under_test.vchan.write_buf, b"test1\0another alpha gamm");
+        assert_eq!(under_test.offset, 0);
+        under_test.write(b" gamma delta").expect("write works");
+        under_test.write(b" gamma delta").expect("write works");
+        under_test.vchan.buffer_space = 8;
+        assert_eq!(under_test.read_header().unwrap(), None, "no bytes to read");
+        under_test.vchan.buffer_space = 8;
+        assert_eq!(under_test.read_header().unwrap(), None, "no bytes to read");
+        under_test.vchan.buffer_space = 8;
+        assert_eq!(under_test.read_header().unwrap(), None, "no bytes to read");
+        under_test.vchan.buffer_space = 8;
+        assert_eq!(under_test.read_header().unwrap(), None, "no bytes to read");
+        assert_eq!(
+            under_test.vchan.write_buf, b"test1\0another alpha gamma delta gamma delta gamma delta",
+            "correct data written"
+        );
     }
 }
